25238bbc18edf763993e8c8bb31f14546546c632
Sushant Kapare <sushantkapare1717@gmail.com>
2024-02-05 12:26:55 +0530

    Create variable.tf

---
+++ Project4_variables/variable.tf
@@ -0,0 +1,15 @@
+variable "client_ip" {
+  default     = "106.195.7.59/32"
+  description = "This is client IP"
+}
+
+variable "ami" {
+
+  default = "ami-0a3c3a20c09d6f377"
+}
+
+variable "instance_type" {
+
+  default = "t2.micro"
+
+}
